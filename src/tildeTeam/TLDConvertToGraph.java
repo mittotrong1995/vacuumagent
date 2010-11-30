6cb372ede60711733450760492bd7a778d65ef9b
psrom1 <psrom1@gmail.com>
2010-11-30 17:56:47 +0000

    very buggy revision

--- src/tildeTeam/TLDConvertToGraph.java
+++ src/tildeTeam/TLDConvertToGraph.java
@@ -48,4 +48,43 @@ public class TLDConvertToGraph {
 		}
 		return graph;
 	}
+	
+	
+	public static SimpleDirectedWeightedGraph<Point, DefaultWeightedEdge> toGraphInnerWorld(TLDInnerWorld floor){
+		SimpleDirectedWeightedGraph<Point, DefaultWeightedEdge> graph = new SimpleDirectedWeightedGraph<Point, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+
+		for(Point p: floor.getFloor().keySet())
+			{
+				VATileStatus status = floor.getTile(p);
+				if(status == VATileStatus.CLEAN || status == VATileStatus.DIRTY)// bhu
+				{
+					graph.addVertex(p);
+				}
+			}
+		
+		for(Point p: graph.vertexSet()){
+		    int x = p.x;
+		    int y = p.y;
+			if (graph.containsVertex(new Point(x+1, y))){
+				graph.addEdge(p,new Point(x+1, y));
+				graph.setEdgeWeight(graph.getEdge(p,new Point(x+1, y)), 1);
+			}
+			if (graph.containsVertex(new Point(x-1, y))){
+				graph.addEdge(p,new Point(x-1, y));
+				graph.setEdgeWeight(graph.getEdge(p,new Point(x-1, y)), 1);
+			}
+			if (graph.containsVertex(new Point(x, y+1))){
+				graph.addEdge(p,new Point(x, y+1));
+				graph.setEdgeWeight(graph.getEdge(p,new Point(x, y+1)), 1);
+			}
+			if (graph.containsVertex(new Point(x, y-1))){
+				graph.addEdge(p,new Point(x, y-1));
+				graph.setEdgeWeight(graph.getEdge(p,new Point(x, y-1)), 1);
+			}
+			
+			
+		}
+		return graph;
+	}
+	
 }
